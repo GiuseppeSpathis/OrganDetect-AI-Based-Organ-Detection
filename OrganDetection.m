f356ac5e296d61f047b1968b07ab0a3494fc04c7
FRDRK <federicoaugelli@gmail.com>
2025-04-07 10:54:23 +0200

    Boiler Plate added

--- OrganDetection.m
+++ OrganDetection.m
@@ -1,6 +1,11 @@
+(* ::Package:: *)
+
+ 
+
+
 (*:Title: OrganDetection *)
 (*:Context: OrganDetection` *)
-(*:Authors: Giuseppe Spathis, ... da sistemare *)
+(*:Authors: Giuseppe Spathis, Federico Augelli, ... *)
 (*:Summary: Organ detection based on ultrasound images using deep learning algorithms, implemented in Wolfram Mathematica. *)
 (*:Copyright: GS 2025 da sistemare *)
 (*:Package Version: 1 *)
@@ -12,3 +17,41 @@
 (*:Discussion: *)
 (*:Requirements: *)
 (*:Warning: package Context is not defined *)
+
+BeginPackage["Object Detection"]
+
+(* Definition of public functions *)
+
+Normalize::usage = "Normalize[f, {x0, x1, ...}]
+	test function for normalizing functions"
+
+FineTune::usage = "FineTune[f, g, h]
+	function for fine tuning the model"
+
+Begin["Private"]
+
+(* Implementation of all function *)
+
+Normalize[] :=
+Module[],
+
+code...
+
+
+
+FineTune[] :=
+Module[],
+
+code...
+
+
+
+(* Defnition of auxiliary functions *)
+
+AuxFunction[] :=
+Module[]
+
+code...
+
+End[]
+EndPackage[]
