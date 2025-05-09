62064882a39183abdfafc6db27009cde827ba864
FRDRK <federicoaugelli@gmail.com>
2025-05-09 08:59:36 +0200

    update script

--- run.sh
+++ run.sh
@@ -10,19 +10,35 @@ ARCH=$(uname -m)
 # Stampa informazioni sul sistema
 INSTALL_DIR="$HOME/miniconda3"
 CONDA_SH="$INSTALL_DIR/miniconda.sh"
+CONDA_BIN="$INSTALL_DIR/bin/conda"
 
 # Installa Miniconda se non è già installato
-if ! command -v conda &> /dev/null; then
+#if ! command -v conda &> /dev/null ; then
+if [[ ! -f "$CONDA_BIN" ]]; then
     echo "Conda is not installed. Installing Miniconda..."
 
     mkdir -p "$INSTALL_DIR"
 
     if [[ "$OS_TYPE" == "Darwin" && "$ARCH" == "arm64" ]]; then
         curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o "$CONDA_SH"
+        bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+        rm ~/miniconda3/miniconda.sh
     elif [[ "$OS_TYPE" == "Darwin" && "$ARCH" == "x86_64" ]]; then
         curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o "$CONDA_SH"
+        bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+        rm ~/miniconda3/miniconda.sh
     elif [[ "$OS_TYPE" == "Linux" && "$ARCH" == "x86_64" ]]; then
         wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$CONDA_SH"
+        # Inizializza Conda
+        echo "Initializing Conda..."
+        source "$INSTALL_DIR/etc/profile.d/conda.sh"
+        conda init bash || true
+        source ~/.bashrc || true
+
+        # Crea l'ambiente Conda e controlla se esiste già
+        echo "Creating Conda environment from environment.yml..."
+        conda activate base
+        conda env create -f environment.yml || echo "Environment may already exist."
     else
         echo "Unsupported OS or architecture. Exiting."
         exit 1
@@ -34,16 +50,6 @@ else
     echo "Conda is installed, proceeding with the script."
 fi
 
-# Inizializza Conda
-echo "Initializing Conda..."
-source "$INSTALL_DIR/etc/profile.d/conda.sh"
-conda init bash || true
-source ~/.bashrc || true
-
-# Crea l'ambiente Conda e controlla se esiste già
-echo "Creating Conda environment from environment.yml..."
-conda activate base
-conda env create -f environment.yml || echo "Environment may already exist."
 
 # Apri il notebook di Mathematica. Se viene aperto con un sistema operativo diverso da MacOS o Linux, mostra un messaggio di errore.
 echo "Attempting to open tutorial.nb..."
